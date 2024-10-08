d9c13ffec6b98657793cabe001b49135f78f6722
Liam <liammtech@gmail.com>
2024-10-08 20:52:32 +0100

    Finish FizzBuzz

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+count = 1;
+
 for (i = 0; i < 100; i++) {
-    count = 1;
-
-
-    console.log(count);
-    count++;
+    if (count % 3 == 0 && count % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (count % 5 == 0) {
+        console.log("Buzz");
+    } else if (count %3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(count);
+    }
+    count += 1;
 }
